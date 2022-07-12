9f53962a4fbf35559be5e4be056a4b980cbd17e2
howe <oeaheh@gmail.com>
2022-07-12 17:42:28 +0800

    Update ParallelQueue.ts
    add stop/pause/resume

--- ParallelQueue.ts
+++ ParallelQueue.ts
@@ -7,7 +7,10 @@
  */
 
 export class ParallelQueue<T> {
-
+    /**
+     * 并行任务状态，0表示正常，1表示暂停
+     */
+    private _state = 0;
     private _taskIndex = 0;
     private _finishCount = 0;
     private _tasks: Array<T> = null;
@@ -31,10 +34,16 @@ export class ParallelQueue<T> {
      * @returns 
      */
     public play(tasks: Array<any>, handler: (taskIndex: number, taskItem: T, workerIndex: number, next: () => void) => void, complete:Function = null) {
+        if (tasks.length === 0) {
+            return complete();
+        }
+        if (!handler) {
+            throw new Error("handler cannot be null！")
+        }
         if (this._taskIndex > 0) {
-            console.log(" ParallelQueue play : 已经有任务正在进行")
             return;
         }
+        this._state = 0;
         this._taskIndex = 0;
         this._finishCount = 0;
         this._tasks = tasks;
@@ -43,11 +52,38 @@ export class ParallelQueue<T> {
         this._doWork();
     }
     /**
+     * stop task
+     * 停止任务队列，如果任务正在执行，则执行完成后，后续任务将清空且不再处理
+     */
+    public stop() {
+        this._tasks && (this._tasks.length = 0);
+    }
+
+    /**
+     * pause task
+     * 暂停执行停止任务队列，如果任务正在执行，则执行完成后，后续任务将暂停，等待调用resume才会继续执行
+     */
+    public pause() {
+        if (this._state === 0) {
+            this._state = 1;
+        }
+    }
+    /**
+     * resume task
+     * 恢复停止的队列
+     */
+    public resume() {
+        if (this._state === 1) {
+            this._state = 0;
+        }
+    }
+    /**
      * 任务执行完成回调
      */
     private _finishWork() {
         this._taskIndex = 0;
         this._finishCount = 0;
+        this._state = 0;
         this._handler = null;
         this._tasks = null;
         this.complete && this.complete();
@@ -57,8 +93,11 @@ export class ParallelQueue<T> {
      * @returns 
      */
     private _doWork() {
-        if (this._taskIndex === this._tasks.length) {
+        if ( this._tasks.length === 0 || this._taskIndex === this._tasks.length) {
             return;
+        }
+        if (this._state === 1) {
+            return; // 任务暂停状态
         }
         if (this._workers.length > 0) {
             let i = this._taskIndex++;
